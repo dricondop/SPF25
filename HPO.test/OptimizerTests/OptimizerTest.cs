4e6035501a4a93176cb111a6260fda3f9e2c4eac
ManuelMaganaMarquez <manuelmaganamarquez@gmail.com>
2025-04-15 11:35:59 +0200

    Test branch creation

--- HPO.test/OptimizerTests/OptimizerTest.cs
+++ HPO.test/OptimizerTests/OptimizerTest.cs
@@ -1,10 +1,11 @@
 ﻿namespace HeatProductionOptimization.test;
-
+using Xunit;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void OptimizeCostProduction() //Basic test to check if the optimizer works only considering costs
     {
+        //Assert
 
     }
     
