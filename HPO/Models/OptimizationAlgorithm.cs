968499357676a1392c7e55529e510fc803c33c88
ManuelMaganaMarquez <manuelmaganamarquez@gmail.com>
2025-04-21 21:09:01 +0200

    Correct Implementation of the Optimization Algorithm in the ViewModel
    Co-authored-by: HS-Carlos <HS-Carlos@users.noreply.github.com>

--- HPO/Models/OptimizationAlgorithm.cs
+++ HPO/Models/OptimizationAlgorithm.cs
@@ -9,24 +9,22 @@ namespace HeatProductionOptimization.Models;
 
 public class OptAlgorithm
 {
-    public Dictionary<double, AssetSpecification> Objective = [];
+    public Dictionary<double, AssetSpecifications> Objective = [];
     
     //The method will ask for the list of specifications and also if the different parameters(par) are to be considered or not.
-    public Dictionary<double, AssetSpecification> GetObjective(List<AssetSpecification> boilers, int[] par, double ElectricityPrice)
+    public Dictionary<double, AssetSpecifications> GetObjective(List<AssetSpecifications> Boilers, int[] par, double ElectricityPrice, List<AssetSpecifications> HeatPumps, List<AssetSpecifications> GasMotors )
     {
-        List<AssetSpecification> Boilers = boilers.Where(n => n.IsActive == true).ToList();
-        List<AssetSpecification> HeatPumps = Boilers.Where( n => n.UnitType == "Motor").ToList();
         foreach(var hp in HeatPumps)
         {
             hp.ProductionCost += ElectricityPrice;
         }
-        List<AssetSpecification> GasMotors =  Boilers.Where( n => n.UnitType == "Heat Pump").ToList();
+        
         foreach(var gm in GasMotors)
         {
             gm.ProductionCost -= ElectricityPrice;
         }
 
-        Dictionary<double, AssetSpecification> obj = [];
+        Dictionary<double, AssetSpecifications> obj = [];
         double objective = 0.0;
 
         int n = par.Where(n => n == 1 ).Count();
@@ -44,9 +42,10 @@ public class OptAlgorithm
         return obj;
     }
 
-    public void CalculateUnits(List<AssetSpecification> boilers, Dictionary<double,AssetSpecification> boilerdict, double heat)
+    public void CalculateUnits(List<AssetSpecifications> Boilers, Dictionary<double,AssetSpecifications> boilerdict, double heat)
     {
-        List<AssetSpecification> Boilers = boilers.Where(n => n.IsActive == true).ToList();
+
+        
         foreach( var boiler in Boilers)
         {
             boiler.UnitsProduction = 0.0;
@@ -79,11 +78,20 @@ public class OptAlgorithm
             }
         }
     }
-    public void CalculateElectricity(List<AssetSpecification> HeatPumps, List<AssetSpecification> GasMotors, double ElectricityPrice)
+    public double? CalculateElectricity(List<AssetSpecifications> HeatPumps, List<AssetSpecifications> GasMotors, double ElectricityPrice)
     {
         double? ElectricityProduced = GasMotors.Sum(n => n.UnitsProduction) * 0.742857;
         double? ElectricityConsumed = HeatPumps.Sum(n => n.UnitsProduction);
         double? Mwh = ElectricityProduced - ElectricityConsumed;
-        double? Cost_Benefit = Mwh * ElectricityPrice;
+        return  Mwh * ElectricityPrice;
+    }
+
+    public void OptimizationAlgorithm(List<AssetSpecifications> boilers, int[] par, double ElectricityPrice,double heat)
+    {
+        List<AssetSpecifications> Boilers = boilers.Where(n => n.IsActive == true).ToList();
+        List<AssetSpecifications> HeatPumps = Boilers.Where( n => n.UnitType == "Heat Pump").ToList();
+        List<AssetSpecifications> GasMotors =  Boilers.Where( n => n.UnitType == "Motor").ToList();
+        CalculateUnits(Boilers, GetObjective(Boilers, par, ElectricityPrice, HeatPumps, GasMotors), heat);
+        CalculateElectricity(HeatPumps, GasMotors, ElectricityPrice); 
     }
 }
